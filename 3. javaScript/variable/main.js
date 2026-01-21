12521ebd7ab2efd0022950097abca71fbf7e442c
Aliyan Shaikh <aliyanzahid02@gmail.com>
2026-01-21 11:41:01 +0500

    add

---
+++ 3. javaScript/variable/main.js
@@ -0,0 +1,23 @@
+//variables:- Chapter 2
+
+// var name = "Aliyan"; //string
+// var age = 22; //number
+// var isStudent = true; //boolean
+
+//Method 1
+alert("Karachi"); // alert as a string
+
+//Method 2
+var cities = "Karachi "; //string
+alert(cities); // alert as a variable
+
+//Method 3
+var cities = "Karachi "; //string
+alert("The City of Light is " + cities); // alert as a variable + String 
+
+//Method 4
+// Two variables addition and show them in alert box
+var cities = "Karachi "; //string
+var discription = "The City of Light";//string
+alert(cities +  discription );
+
