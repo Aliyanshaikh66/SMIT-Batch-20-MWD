94d080d8445c2aa0eeaa47e18ceaaa8bdb136879
Aliyan Shaikh <aliyanzahid02@gmail.com>
2026-03-04 10:21:01 +0500

    css grid

---
+++ 3. javaScript/8. For Loop/main.js
@@ -0,0 +1,92 @@
+// for loop Simple Example
+// var users = ["Aliyan", "Asif", "Ahad"];
+// for (var i = 0; i < users.length; i++) {
+//     console.log(users[i]);
+// } 
+
+// var i = 0;
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i);
+//   // some statements
+// }
+
+
+
+
+// for loop with break statement
+// var users = ["Aliyan", "Asif", "Ahad", "Basheer", "Hamid"];
+// for (var i = 0; i = users.length; i++) {
+//         if (users[i] === "Hammad") {
+//         alert("Hammad is found");
+//         }
+//         else{
+//             alert("Hammad is not found");
+//             break;
+//         }
+        
+        // else if (users[i] === "Asif") {
+        // alert("Asif is found");
+        //     break;
+
+
+// for loop with index
+// var users = ["Aliyan", "Asif", "Ahad"];
+// for (var i = 0; i < users.length; i++) {
+//     console.log(i + ": " + users[i]);
+// }
+
+// for loop with reverse index
+// var users = ["Aliyan", "Asif", "Ahad"];
+// for (var i = users.length - 1; i >= 0; i--) {
+//     console.log(i + ": " + users[i]);
+// }
+
+
+
+
+       
+        // else if (users[i] === "Hamid") {
+        // alert("Hamid is found");
+        //     break;
+        // }
+        // else if (users[i] === "Ahad") {
+        // alert("Ahad is found");
+        // break;
+        // }     
+       
+    // console.log(users[i]);
+
+
+
+
+
+
+
+
+
+// for loop with continue statement
+// var users = ["Aliyan", "Asif", "Ahad"];
+// for (var i = 0; i < users.length; i++) {
+//     if (users[i] === "Asif") {
+//         continue;
+//     }
+//     console.log(users[i]);
+// }
+
+var cityToCheck = prompt("Enter a city name");
+var cleanestCities = ["Karachi", "Santa Fe", "Tucson", "Great Falls", "Honolulu"];
+ var matchFound = "no";
+ for (var i = 0; i <= 5; i++) {
+ if (cityToCheck === cleanestCities[i]) {
+ matchFound! = "yes";
+ alert("It's one of the cleanest cities");
+ }
+ }
+ if (matchFound === "no") {
+ alert("It's not on the list");
+ }
+
+
+
+  
+
