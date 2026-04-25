77ef830898f35eeb0b15e519b544316d4ebe7262
Aliyanshaikh66 <aliyanzahid02@gmail.com>
2026-04-25 10:57:08 +0500

    if else & esle if statement with nestefd if statement

--- 3. javaScript/5. if else & else if statements/main.js
+++ 3. javaScript/5. if else & else if statements/main.js
@@ -99,3 +99,109 @@
 // cities.push(yourcity);
 // console.log(cities.length);
 
+// chapter No:9 if else statements
+
+// if ( 1223 === 1223) {
+//     console.log("This is true");
+// }
+
+// let user = "Jawad";
+// if (user === "Jawad"){
+//     console.log("Jawad is True...!");  
+// } else {
+//     console.log("Jawad is False...!");
+// }
+
+// let user = prompt("Enter your name using lowercase letters");
+// if (user == "jawad"){
+//     console.log("Jawad is True...!");  
+// } else if (user == "adnan"){
+//     console.log("Adnan is True...!");  
+// }
+// else{
+//     console.log("User is not found...!");
+// }
+
+// let num = 20;
+// let num1 = 10;
+// if (num < 30){
+//     console.log("true");
+// } else{
+//     console.log("false");
+// }
+
+
+// chapter No:13 Logical Operators
+
+// and operators
+// let num1 = 5;
+// let num2 = 10;
+// if (num1 > 0 && num2 >= 10) {
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }  
+
+// or operators
+
+// let num1 = 5;
+// let num2 = 10;
+// if (num1 > 6 || num2 >= 10) {
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
+
+// nested if statements
+// if ((10 > 20 && 10 < 20)){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+// if ((10 > 20 || 21 < 20) || (10 > 5 || 6 < 5)){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+let num1 = 5;
+let num2 = 10;
+
+if (num1 == 6 ){
+    console.log("true");
+    if (num2 == 10){
+        console.log("true1");
+    }else{
+        console.log("false1");
+    }
+}else{
+    console.log("false");  
+}
+
+
+
+// if (10 < 20){
+//     console.log("true");
+//     if (10 < 20){
+//         console.log("true1");
+//     }
+//     else{
+//         console.log("false1");
+//     }
+// }else{
+//     console.log("false");
+// }
+
+
+
+// Chapter No:10 Comparison Operators
+// let a = 5;
+// let b = 10;
+// // let c = b === 11;
+// let d = b < 10;
+// // console.log(a < b);
+// console.log(d);
+// // console.log("This Is True");
